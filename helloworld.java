ca38df864668e282ac55ad5218bce487c9991778
Feroz196 <98863206+Feroz196@users.noreply.github.com>
2022-02-09 07:43:36 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World!! poll scheduling");
+    System.out.println("Trigger by GitHub webhook");
     }
 }
